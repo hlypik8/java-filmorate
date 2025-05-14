18a8f7d89e60cabdad31e3f61fbaed34e75c3e55
hlypik <arsenyborovskoi@yandex.ru>
2025-05-15 01:36:50 +0300

    Правки кодстайла

--- src/test/java/ru/yandex/practicum/filmorate/modelTest/FilmTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/modelTest/FilmTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.time.Duration;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
