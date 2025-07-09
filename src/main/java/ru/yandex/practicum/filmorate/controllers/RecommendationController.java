e23b0009eae1d4c56207cf0e4bd661b59c65e1b5
Ilia Levin <lena.pro.99@bk.ru>
2025-07-09 17:08:59 +0300

    add-recommandation: i did recommandation methods and Controller for recommandation

---
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/RecommendationController.java
@@ -0,0 +1,30 @@
+package ru.yandex.practicum.filmorate.controllers;
+
+// RecommendationController.java
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users/{id}")
+public class RecommendationController {
+
+    private final FilmService filmService;
+
+    public RecommendationController(FilmService filmService) {
+        this.filmService = filmService;
+    }
+
+    @GetMapping("/recommendations")
+    public ResponseEntity<List<Film>> getRecommendations(@PathVariable int id) {
+        List<Film> recommendations = filmService.getRecommendations(id);
+        return ResponseEntity.ok(recommendations);
+    }
+}
--- src/main/java/ru/yandex/practicum/filmorate/storage/likesStorage/LikesDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/likesStorage/LikesDbStorage.java
@@ -1,35 +1,57 @@
-package ru.yandex.practicum.filmorate.storage.likesStorage;
+    package ru.yandex.practicum.filmorate.storage.likesStorage;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
-import ru.yandex.practicum.filmorate.storage.BaseStorage;
+    import org.springframework.jdbc.core.JdbcTemplate;
+    import org.springframework.stereotype.Repository;
+    import ru.yandex.practicum.filmorate.storage.BaseStorage;
 
-@Repository
-public class LikesDbStorage extends BaseStorage<Integer> {
+    import java.util.List;
+
+    @Repository
+    public class LikesDbStorage extends BaseStorage<Integer> {
 
 
-    public LikesDbStorage(JdbcTemplate jdbcTemplate) {
-        super(jdbcTemplate);
-    }
+        public LikesDbStorage(JdbcTemplate jdbcTemplate) {
+            super(jdbcTemplate);
+        }
 
-    public void addLike(int userId, int filmId) {
+        public void addLike(int userId, int filmId) {
 
-        String query = """
-                INSERT INTO likes (user_id, film_id)
-                VALUES (?, ?);
+            String query = """
+                    INSERT INTO likes (user_id, film_id)
+                    VALUES (?, ?);
+                    """;
+
+            insert(query, userId, filmId);
+        }
+
+        public void removeLike(int userId, int filmId) {
+
+            String query = """
+                    DELETE FROM likes
+                    WHERE user_id = ?
+                          AND film_id = ?;
+                    """;
+
+            delete(query, userId, filmId);
+        }
+
+        // Получаем все фильмы, которые лайкнул пользователь (возвращаем список film_id)
+        public List<Integer> getLikesByUser(int userId) {
+            String query = """
+                SELECT film_id
+                FROM likes
+                WHERE user_id = ?;
                 """;
+            return jdbcTemplate.queryForList(query, Integer.class, userId);
+        }
 
-        insert(query, userId, filmId);
-    }
-
-    public void removeLike(int userId, int filmId) {
-
-        String query = """
-                DELETE FROM likes
-                WHERE user_id = ?
-                      AND film_id = ?;
+        // Получаем всех пользователей, которые лайкнули конкретный фильм
+        public List<Integer> getUsersWhoLikedFilm(int filmId) {
+            String query = """
+                SELECT user_id
+                FROM likes
+                WHERE film_id = ?;
                 """;
-
-        delete(query, userId, filmId);
+            return jdbcTemplate.queryForList(query, Integer.class, filmId);
+        }
     }
-}
